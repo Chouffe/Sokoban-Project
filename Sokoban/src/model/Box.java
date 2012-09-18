b11094daa84c375d66d6b88a8d5716c55f301ee6
Alden Coots <coots@csc.kth.se>
2012-09-18 11:18:49 +0200

    Renamed Block to Box

--- Sokoban/src/model/Box.java
+++ Sokoban/src/model/Box.java
@@ -1,18 +1,8 @@
 package model;
 
 public class Box {
-	
-	protected Map map;
 	protected Position position;
 	protected boolean onGoal;
-
-	Box(Map m, Position pos) {
-		map = m;
-		position = pos;
-		if (m.getGoals().contains(pos)) {
-			onGoal = true;
-		}
-	}
 
 	Box(Map m, Position pos, boolean goal) {
 		map = m;
